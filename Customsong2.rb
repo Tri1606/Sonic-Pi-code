8ef495e6ccd3b9cc6f68616751f137a41d47e5c3
Tri1606 <thientri.nguyen2020@gmail.com>
2026-03-30 13:16:51 -0700

    Update Customsong2.rb

--- Customsong2.rb
+++ Customsong2.rb
@@ -1,38 +1,119 @@
-use_bpm 60
-
+use_bpm 56.94
 use_synth :piano
+beat_1="C:/Users/momen/Downloads/G Dragon - Black Recreated  Piano and Beat tutorial Y-one short na 1.wav"
+beat_2="C:/Users/momen/Downloads/G Dragon - Black Recreated  Piano and Beat tutorial Y-one short nn 2.wav"
+beat_3="C:/Users/momen/Downloads/G Dragon - Black Recreated  Piano and Beat tutorial Y-one short nh 3.wav"
+
+wonky="C:/Users/momen/Downloads/Wonky.wav"
+
+vocal="C:/Users/momen/Downloads/Nice.wav"
+
+transition_rain="C:/Users/momen/Downloads/Real rain.wav"
+
+
+transition_walking="C:/Users/momen/Downloads/Real walking.wav"
+
+
+Fmaj7=[:F2, :F3, :A3, :C4, :E4]
+Cmaj=[:C2, :C3, :G3, :C3, :E2]
+Emin7=[:E2, :E3, :G3, :B3, :D4]
+Amaj7=[:A2, :A3, :G3, :B3, :D4]
+
+define :beat1 do
+  sample beat_1, sustain_level: 0.4, amp: 14
+end
+
+define :beat2 do
+  sample beat_2, sustain_level: 0.2, amp: 16
+end
+
+define :beat3 do
+  sample beat_3, sustain_level: 0.3, amp: 16
+end
+
+
+define :play_my_chord do |chord, slp|
+  play_chord chord, sustain: 4, release: 4, amp: 5
+  sleep slp
+end
+
+# Fade-out for transition_rain
+amp_level = 1    # start at full volume
+fade_steps = 5   # number of steps for fade
+
+fade_steps.times do
+  sample transition_rain, amp: amp_level
+  sleep 0.4      # adjust for smoothness
+  amp_level = amp_level - 0.2   # decrease amp gradually
+end
+
+sleep 2   # original sleep after rain sample
+
+# Fade-out for transition_walking
+amp_level = 4    # start at full volume
+fade_steps = 8   # more steps for smoother fade
+
+fade_steps.times do
+  sample transition_walking, amp: amp_level
+  sleep 0.5
+  amp_level = amp_level - 0.5
+end
+
+sleep 4   # original sleep after walking sample
 live_loop :chords do
-  with_fx :reverb, mix: -0, sync: :drum, room: 1 do
-    play_chord [:F2, :F3, :A3, :C3, :E4], sustain: 400, release: 400, amp: 6
-    sleep 2.65
-    play_chord [:C2, :C3, :G3, :C3, :E3], sustain: 400, release: 400, amp: 6
-    sleep 2.65
-    play_chord [:E2, :E3, :G3, :B3, :D4], sustain: 400, release: 400, amp: 6
-    sleep 2.65
-    play_chord [:A2, :A3, :G3, :B3, :D4], sustain: 400, release: 400, amp: 6
-    sleep 0.98
-    play :Cs4, sustain: 300, release: 400, amp: 1.5
-    sleep 1
-    play :A3, sustain: 300, release: 400, amp: 1.5
-    sleep 0.66
+  with_fx :reverb, mix: 0.4, room: 1 do
+    16.times do
+      
+      play_my_chord Fmaj7, 2.53
+      play_my_chord Cmaj, 2.53
+      play_my_chord Emin7, 2.53
+      play_my_chord Amaj7, 0.95
+      
+      play :Cs4, sustain: 3, release: 4, amp: 1.5
+      sleep 0.96
+      
+      play :A3, sustain: 3, release: 4, amp: 1.5
+      sleep 0.62
+    end
+    stop
   end
 end
 
 live_loop :drum do
-  sample :bd_klub, sustain_level: 0.4, amp: 3
-  sleep 0.67
-  sample :sn_dolf, sustain_level: 0.2, amp: 2
-  sleep 0.36
-  sample :bd_fat, sustain_level: 2, amp: 2
-  sleep 0.17
-  sample :bd_fat, sustain_level: 0.3, amp: 2
-  sample :bd_klub, sustain_level: 0.4, amp: 3
-  sleep 0.45
-  sample :bd_fat, sustain_level: 0.5, amp: 2
-  sample :bd_klub, sustain_level: 0.4, amp: 3
-  sleep 0.34
-  sample :sn_dolf, sustain_level: 0.2, amp: 2
-  sleep 0.16
-  sample :bd_fat, sustain_level: 2, amp: 2
-  sleep 0.5
+  56.times do
+    beat1
+    sleep 0.65
+    beat2
+    sleep 0.34
+    beat3
+    sleep 0.16
+    beat3
+    beat1
+    sleep 0.43
+    beat3
+    beat1
+    sleep 0.32
+    beat2
+    sleep 0.15
+    beat3
+    sleep 0.48
+  end
+  stop
+end
+
+sleep 20.1
+
+sample vocal, sync: :piano , amp: 1.3
+
+sleep 9.6
+
+amp_level = 0
+
+live_loop :wonky_sound do
+  14.times do
+    sample wonky, amp: amp_level, release: 10
+    sleep 10
+    amp_level = [amp_level + 0.5, 3.5].min
+  end
+  stop
 end
